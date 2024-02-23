c8de294bbc801740ae7f15c953b870dad51d439f
shanibinaryops <shayan@binaryops.co>
2024-02-23 14:45:50 +0500

    for demo purposes checking commit and pull

---
+++ app/src/main/java/com/example/firebase/Auth/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.firebase.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebase.R
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
---
+++ app/src/main/java/com/example/firebase/Auth/SignupScreen.kt
@@ -0,0 +1,12 @@
+package com.example.firebase.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebase.R
+
+class SignupScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_screen)
+    }
+}
