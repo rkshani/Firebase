3b8beae5a3bbed346bc95e1e6e9137bfe160aa42
shanibinaryops <shayan@binaryops.co>
2024-03-11 15:33:38 +0500

    textviews

---
+++ app/src/main/java/com/example/firebase/Android/TextViews/SpannableTextViews.kt
@@ -0,0 +1,16 @@
+package com.example.firebase.Android.TextViews
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firebase.R
+
+class SpannableTextViews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spannable_text_views)
+
+    }
+}
